31c53044802952968f246b52b81c0c58233fd8a4
Jackson Li <jacksonli_@outlook.com>
2024-06-19 14:59:36 +1000

    Add basic functions for operations

---
+++ script.js
@@ -0,0 +1,32 @@
+const firstNum = XMLDocument;
+const secondNum = y;
+const operator = x;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(operator, a, b){
+        switch(operator){
+            case "+":
+                return add(a, b);
+            case "-":
+                return subtract(a, b);
+            case "*":
+                return multiply(a, b);
+            case "/":
+                return divide(a, b);
+        }
+}
