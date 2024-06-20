1c5fcc8c54426fb26897c386bf58c7e3642e2aa8
Jackson Li <jacksonli_@outlook.com>
2024-06-20 13:03:49 +1000

    Add functionality for buttons, format the buttons to match Apple calculator as inspiration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const firstNum = XMLDocument;
-const secondNum = y;
-const operator = x;
+let firstNumber;
+let operator;
+let secondNumber;
 
 function add(a, b){
     return a + b;
@@ -15,10 +15,13 @@ function multiply(a, b){
 }
 
 function divide(a, b){
-    return a / b;
+    if (b == 0){
+        return "Undefined";
+    };
+    return (a / b).toFixed(10);
 }
 
-function operate(operator, a, b){
+function operate(a, operator, b){
         switch(operator){
             case "+":
                 return add(a, b);
@@ -30,3 +33,65 @@ function operate(operator, a, b){
                 return divide(a, b);
         }
 }
+
+
+function updateValue(num){
+    const output = document.querySelector(".output");
+    if (output.textContent.includes('.')){
+        let rightValues = countDecimal(output.textContent);
+        output.textContent = parseFloat(output.textContent) + parseFloat(num) * 0.1 / 10**rightValues;
+    } else {
+    output.textContent = parseFloat(output.textContent) * 10 + parseFloat(num);
+    }
+}
+
+function countDecimal(num){
+    const decimalIndex = num.indexOf('.');
+    return num.length - decimalIndex -1;
+}
+
+function resetValue(){
+    const output = document.querySelector(".output");
+    output.textContent = 0;
+}
+
+function changeSign(){
+    const output = document.querySelector(".output");
+    output.textContent = parseFloat(-output.textContent);
+}
+
+function percentagise(){
+    const output = document.querySelector(".output");
+    output.textContent = divide(parseFloat(output.textContent), 100);
+}
+
+function addDot(){
+    const output = document.querySelector(".output");
+    output.textContent = parseFloat(output.textContent)+".";
+}
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+  // Add a click listener
+  number.addEventListener("click", () => {
+    updateValue(number.textContent);
+});
+});
+
+
+const dot = document.querySelector(".dot");
+dot.addEventListener("click", () => {
+addDot()});
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+resetValue()});
+
+const sign = document.querySelector(".sign");
+sign.addEventListener("click", () => {
+changeSign()});
+
+const percentage = document.querySelector(".percentage");
+percentage.addEventListener("click", () => {
+percentagise()});
+
